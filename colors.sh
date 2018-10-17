85d62f60b6a35b1fb457ba34cc2770a7d3801a04
BlueArduino20 <bluearduino20@gmail.com>
2018-10-17 12:43:56 +0200

    Update colors.sh

--- colors.sh
+++ colors.sh
@@ -1,46 +1,28 @@
-# TERMINAL COLORS:
+# TERMINAL COLORS
+# https://github.com/BlueArduino20/terminal-colors
 NO_COLOR="\e[0m"
-WHITE="\e[0;37m"
+WHITE="\e[0;17m"
+BOLD_WHITE="\e[1;37m"
 BLACK="\e[0;30m"
 BLUE="\e[0;34m"
-LIGHT_BLUE="\e[0;34m"
+BOLD_BLUE="\e[1;34m"
 GREEN="\e[0;32m"
-LIGHT_GREEN="\e[0;32m"
+BOLD_GREEN="\e[1;32m"
 CYAN="\e[0;36m"
-LIGHT_CYAN="\e[0;36m"
+BOLD_CYAN="\e[1;36m"
 RED="\e[0;31m"
-LIGHT_RED="\e[0;31m"
+BOLD_RED="\e[1;31m"
 PURPLE="\e[0;35m"
-LIGHT_PURPLE="\e[0;35m"
+BOLD_PURPLE="\e[1;35m"
 BROWN="\e[0;33m"
-YELLOW="\e[0;33m"
-GRAY="\e[0;30m"
-LIGHT_GRAY="\e[0;37m"
-LIGHT_GRAY="\e[0;37m"
-# BOLD COLORS (B means BOLD):
-B_WHITE="\e[1;37m"
-B_BLACK="\e[1;30m"
-B_BLUE="\e[1;34m"
-B_LIGHT_BLUE="\e[1;34m"
-B_GREEN="\e[1;32m"
-B_LIGHT_GREEN="\e[1;32m"
-B_CYAN="\e[1;36m"
-B_LIGHT_CYAN="\e[1;36m"
-B_RED="\e[1;31m"
-B_LIGHT_RED="\e[1;31m"
-B_PURPLE="\e[1;35m"
-B_LIGHT_PURPLE="\e[1;35m"
-B_BROWN="\e[1;33m"
-B_YELLOW="\e[1;33m"
-B_GRAY="\e[1;30m"
-B_LIGHT_GRAY="\e[1;37m"
-B_LIGHT_GRAY="\e[1;37m"
+BOLD_YELLOW="\e[1;33m"
+GRAY="\e[0;37m"
+BOLD_GRAY="\e[1;30m"
 # END OF TERMINAL COLORS
 
-# USAGE:
-# echo -e $YELLOW
+# // USAGE: //
+# echo -e $CYAN
 # echo "I'm a yellow text"
-# echo -e $B_YELLOW
-# echo "I'm a bold yellow text"
+# echo -e "$GREEN I'm a green text"
 
 # [ YOUR COLORFUL CODE HERE ]
